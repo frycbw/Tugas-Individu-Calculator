1bbb30f228569f3f56c0f3f3aade1b2286306d8b
frycbw <65535515+frycbw@users.noreply.github.com>
2020-05-31 08:41:37 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_gua
+{
+    public partial class Form1 : Form
+    {
+        String operation = "";
+        Double firstnum, secondnum;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+
+            String f, s;
+
+            s = Convert.ToString(secondnum);
+            f = Convert.ToString(firstnum);
+
+            s = "";
+            f = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text.Length > 0)
+            {
+                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
+            }
+
+            if (txtDisplay.Text == "")
+            {
+                txtDisplay.Text = "0";
+            }
+        }
+
+        private void NumericValue(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            if (txtDisplay.Text == "0")
+                txtDisplay.Text = "";
+
+            if (b.Text == ",")
+            {
+                if (!txtDisplay.Text.Contains(","))
+                    txtDisplay.Text = txtDisplay.Text + b.Text;
+            }
+
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + b.Text;
+            }
+        }
+
+        private void txtDisplay_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Operatonal_Function(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            firstnum = Double.Parse(txtDisplay.Text);
+            operation = b.Text;
+            txtDisplay.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!txtDisplay.Text.Contains("-"))
+            {
+                txtDisplay.Text = txtDisplay.Text.Remove(0);
+            }
+
+            else
+            {
+                txtDisplay.Text = "-" + txtDisplay.Text;
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            secondnum = Double.Parse(txtDisplay.Text);
+
+            switch (operation)
+            {
+                case "+":
+                    txtDisplay.Text = Convert.ToString(firstnum + secondnum);
+                    break;
+                case "-":
+                    txtDisplay.Text = Convert.ToString(firstnum - secondnum);
+                    break;
+                case "×":
+                    txtDisplay.Text = Convert.ToString(firstnum * secondnum);
+                    break;
+                case "÷":
+                    txtDisplay.Text = Convert.ToString(firstnum / secondnum);
+                    break;
+                
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+        }
+    }
+}
